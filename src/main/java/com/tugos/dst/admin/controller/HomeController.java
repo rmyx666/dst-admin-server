49821b14455efa161674217b095dadca9da4c584
wang.guanru@trs.com.cn <Wang_guanru>
2024-02-29 15:16:16 +0800

    将跳转部分限制放开，未调整完的接口无返回值

--- src/main/java/com/tugos/dst/admin/controller/HomeController.java
+++ src/main/java/com/tugos/dst/admin/controller/HomeController.java
@@ -1,219 +1,230 @@
-//package com.tugos.dst.admin.controller;
-//
-//
-//import com.tugos.dst.admin.common.ResultVO;
+package com.tugos.dst.admin.controller;
+
+
+import com.tugos.dst.admin.common.ResultVO;
 //import com.tugos.dst.admin.service.BackupService;
 //import com.tugos.dst.admin.service.HomeService;
 //import com.tugos.dst.admin.service.ShellService;
-//import com.tugos.dst.admin.vo.DstServerInfoVO;
-//import com.tugos.dst.admin.vo.GameArchiveVO;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.shiro.authz.annotation.RequiresAuthentication;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.i18n.LocaleContextHolder;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import javax.servlet.http.HttpServletRequest;
-//import java.util.List;
-//import java.util.Locale;
-//import java.util.Map;
-//
-///**
-// * @author qinming
-// * @date 2020-05-17
-// * <p> 游戏管理器 </p>
-// */
-//@Controller
-//@Slf4j
-//@RequestMapping("/home")
-//public class HomeController {
-//
+import com.tugos.dst.admin.vo.DstServerInfoVO;
+import com.tugos.dst.admin.vo.GameArchiveVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author qinming
+ * @date 2020-05-17
+ * <p> 游戏管理器 </p>
+ */
+@Controller
+@Slf4j
+@RequestMapping("/home")
+public class HomeController {
+
 //    private HomeService homeService;
 //    private ShellService shellService;
 //    private BackupService backupService;
-//
-//
-//    /**
-//     * 首页
-//     */
-//    @GetMapping("/index")
-//    @RequiresAuthentication
-//    public String index(HttpServletRequest request) {
-//        Locale locale = LocaleContextHolder.getLocale();
-//        if (Locale.CHINA.getLanguage().equals(locale.getLanguage())) {
-//            request.setAttribute("lang", "zh");
-//        } else {
-//            request.setAttribute("lang", "en");
-//        }
-//        log.info("访问首页");
-//        return "/home/index";
-//    }
-//
-//
-//    /**
-//     * 获取服务器的信息
-//     * 包括饥荒状态和硬件信息
-//     */
-//    @GetMapping("/getSystemInfo")
-//    @ResponseBody
-//    @RequiresAuthentication
-//    public ResultVO<DstServerInfoVO> getSystemInfo(@RequestParam(required = true) String roomId) throws Exception {
-//        log.debug("获取服务器的信息");
+
+
+    /**
+     * 首页
+     */
+    @GetMapping("/index")
+    @RequiresAuthentication
+    public String index(HttpServletRequest request) {
+        Locale locale = LocaleContextHolder.getLocale();
+        if (Locale.CHINA.getLanguage().equals(locale.getLanguage())) {
+            request.setAttribute("lang", "zh");
+        } else {
+            request.setAttribute("lang", "en");
+        }
+        log.info("访问首页");
+        return "/home/index";
+    }
+
+
+    /**
+     * 获取服务器的信息
+     * 包括饥荒状态和硬件信息
+     */
+    @GetMapping("/getSystemInfo")
+    @ResponseBody
+    @RequiresAuthentication
+    public ResultVO<DstServerInfoVO> getSystemInfo(@RequestParam(required = true) String roomId) throws Exception {
+        log.debug("获取服务器的信息");
 //        return ResultVO.data(homeService.getSystemInfo(roomId));
-//    }
-//
-//    @GetMapping("/start")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> start(@RequestParam Integer type,
-//                                  @RequestParam(required = true) String roomId) throws Exception {
-//        log.info("启动服务器，type={}", type);
+        return ResultVO.success();
+    }
+
+    @GetMapping("/start")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> start(@RequestParam Integer type,
+                                  @RequestParam(required = true) String roomId) throws Exception {
+        log.info("启动服务器，type={}", type);
 //        return homeService.start(type,roomId);
-//    }
-//
-//
-//    @GetMapping("/stop")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> stop(@RequestParam Integer type,@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("停止服务器，type={}", type);
+        return ResultVO.success();
+    }
+
+
+    @GetMapping("/stop")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> stop(@RequestParam Integer type,@RequestParam(required = true) String roomId) throws Exception {
+        log.info("停止服务器，type={}", type);
 //        return homeService.stop(type,roomId);
-//    }
-//
-//    @GetMapping("/updateGame")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> updateGame(@RequestParam(required = true) String roomId) {
-//        log.info("更新游戏");
+        return ResultVO.success();
+    }
+
+    @GetMapping("/updateGame")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> updateGame(@RequestParam(required = true) String roomId) {
+        log.info("更新游戏");
 //        return homeService.updateGame(roomId);
-//    }
-//
-//
-//    @GetMapping("/backup")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> backup(@RequestParam(required = false) String name,
-//                                   @RequestParam(required = true) String roomId) throws Exception {
-//        log.info("备份游戏,{}", name);
+        return ResultVO.success();
+    }
+
+
+
+    @GetMapping("/backup")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> backup(@RequestParam(required = false) String name,
+                                   @RequestParam(required = true) String roomId) throws Exception {
+        log.info("备份游戏,{}", name);
 //        return backupService.backup(name,roomId);
-//    }
-//
-//    @GetMapping("/restore")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> restore(@RequestParam String name,@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("恢复存档,{}", name);
+        return ResultVO.success();
+    }
+
+    @GetMapping("/restore")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> restore(@RequestParam String name,@RequestParam(required = true) String roomId) throws Exception {
+        log.info("恢复存档,{}", name);
 //        homeService.stopServer();
 //        return backupService.restore(name,roomId);
-//    }
-//
-//    @GetMapping("/delRecord")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> delRecord(@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("清理游戏记录");
+        return ResultVO.success();
+    }
+
+    @GetMapping("/delRecord")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> delRecord(@RequestParam(required = true) String roomId) throws Exception {
+        log.info("清理游戏记录");
 //        homeService.delRecord(roomId);
-//        Thread.sleep(1000);
-//        return ResultVO.success();
-//    }
-//
-//    @GetMapping("/sendBroadcast")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> sendBroadcast(@RequestParam String message,@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("发送公告：" + message);
+        Thread.sleep(1000);
+        return ResultVO.success();
+
+    }
+
+    @GetMapping("/sendBroadcast")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> sendBroadcast(@RequestParam String message,@RequestParam(required = true) String roomId) throws Exception {
+        log.info("发送公告：" + message);
 //        shellService.sendBroadcast(message,roomId);
-//        Thread.sleep(1000);
-//        return ResultVO.success();
-//    }
-//
-//    @GetMapping("/getPlayerList")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<List<String>> getPlayerList(@RequestParam(required = true) String roomId) throws Exception {
+        Thread.sleep(1000);
+        return ResultVO.success();
+    }
+
+    @GetMapping("/getPlayerList")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<List<String>> getPlayerList(@RequestParam(required = true) String roomId) throws Exception {
 //        List<String> playerList = shellService.getPlayerList(roomId);
 //        return ResultVO.data(playerList);
-//    }
-//
-//    @GetMapping("/kickPlayer")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> kickPlayer(@RequestParam String userId,@RequestParam(required = true) String roomId) {
-//        log.info("踢出玩家：" + userId);
+        return ResultVO.success();
+    }
+
+    @GetMapping("/kickPlayer")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> kickPlayer(@RequestParam String userId,@RequestParam(required = true) String roomId) {
+        log.info("踢出玩家：" + userId);
 //        shellService.kickPlayer(userId,roomId);
-//        return ResultVO.success();
-//    }
-//
-//    @GetMapping("/rollback")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> rollback(@RequestParam Integer dayNum,@RequestParam(required = true) String roomId) {
-//        log.info("回滚指定的天数：" + dayNum);
+        return ResultVO.success();
+    }
+
+    @GetMapping("/rollback")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> rollback(@RequestParam Integer dayNum,@RequestParam(required = true) String roomId) {
+        log.info("回滚指定的天数：" + dayNum);
 //        shellService.rollback(dayNum,roomId);
-//        return ResultVO.success();
-//    }
-//
-//    @GetMapping("/regenerate")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> regenerate(@RequestParam(required = true) String roomId) {
-//        log.info("重置世界...");
+        return ResultVO.success();
+    }
+
+    @GetMapping("/regenerate")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> regenerate(@RequestParam(required = true) String roomId) {
+        log.info("重置世界...");
 //        shellService.regenerate(roomId);
-//        return ResultVO.success();
-//    }
-//
-//    @PostMapping("/masterConsole")
-//    @ResponseBody
-//    @RequiresAuthentication
-//    public ResultVO<String> masterConsole(@RequestBody Map<String, String> param,@RequestParam(required = true) String roomId) {
+        return ResultVO.success();
+    }
+
+    @PostMapping("/masterConsole")
+    @ResponseBody
+    @RequiresAuthentication
+    public ResultVO<String> masterConsole(@RequestBody Map<String, String> param,@RequestParam(required = true) String roomId) {
 //        shellService.masterConsole(param.get("command"),roomId);
-//        return ResultVO.success();
-//    }
-//
-//    @PostMapping("/cavesConsole")
-//    @ResponseBody
-//    @RequiresAuthentication
-//    public ResultVO<String> cavesConsole(@RequestBody Map<String, String> param,@RequestParam(required = true) String roomId) {
+        return ResultVO.success();
+    }
+
+    @PostMapping("/cavesConsole")
+    @ResponseBody
+    @RequiresAuthentication
+    public ResultVO<String> cavesConsole(@RequestBody Map<String, String> param,@RequestParam(required = true) String roomId) {
 //        shellService.cavesConsole(param.get("command"),roomId);
-//        return ResultVO.success();
-//    }
-//
-//    @GetMapping("/playerOperate")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> playerOperate(@RequestParam String userId, @RequestParam String type,@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("执行高级针对玩家的操作：type={},userId={}", type, userId);
+        return ResultVO.success();
+    }
+
+    @GetMapping("/playerOperate")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> playerOperate(@RequestParam String userId, @RequestParam String type,@RequestParam(required = true) String roomId) throws Exception {
+        log.info("执行高级针对玩家的操作：type={},userId={}", type, userId);
 //        return shellService.playerOperate(type, userId,roomId);
-//    }
-//
-//    @GetMapping("/delMyDediServer")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> delMyDediServer(@RequestParam(required = true) String roomId) {
-//        log.info("删除MyDediServer目录");
+        return ResultVO.success();
+    }
+
+    @GetMapping("/delMyDediServer")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> delMyDediServer(@RequestParam(required = true) String roomId) {
+        log.info("删除MyDediServer目录");
 //        homeService.delMyDediServer(roomId);
-//        return ResultVO.success();
-//    }
-//
-//    @GetMapping("/delCavesRecord")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> onlyDelSave(@RequestParam(required = true) String roomId) {
-//        log.info("删除地面存档记录");
+        return ResultVO.success();
+    }
+
+    @GetMapping("/delCavesRecord")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> onlyDelSave(@RequestParam(required = true) String roomId) {
+        log.info("删除地面存档记录");
 //        homeService.onlyDelSave(roomId);
-//        return ResultVO.success();
-//    }
-//
-//    @GetMapping("/getGameArchive")
-//    @ResponseBody
-//    @RequiresAuthentication
-//    public ResultVO<GameArchiveVO> getGameArchive(@RequestParam(required = true) String roomId) throws Exception {
+        return ResultVO.success();
+    }
+
+    @GetMapping("/getGameArchive")
+    @ResponseBody
+    @RequiresAuthentication
+    public ResultVO<GameArchiveVO> getGameArchive(@RequestParam(required = true) String roomId) throws Exception {
 //        return ResultVO.data(homeService.getGameArchive(roomId));
-//    }
-//
-//
-//
+        return ResultVO.success();
+    }
+
+
+
 //    @Autowired
 //    public void setHomeService(HomeService homeService) {
 //        this.homeService = homeService;
@@ -228,4 +239,4 @@
 //    public void setBackupService(BackupService backupService) {
 //        this.backupService = backupService;
 //    }
-//}
+}
--- src/main/java/com/tugos/dst/admin/controller/PlayerController.java
+++ src/main/java/com/tugos/dst/admin/controller/PlayerController.java
@@ -1,82 +1,86 @@
-//package com.tugos.dst.admin.controller;
-//
-//import com.tugos.dst.admin.common.ResultVO;
-//import com.tugos.dst.admin.service.PlayerService;
-//import com.tugos.dst.admin.vo.PlayerSettingVO;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.shiro.authz.annotation.RequiresAuthentication;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-///**
-// * @author qinming
-// * @date 2020-11-16 22:46:07
-// * <p> 玩家设置，设置黑白名单，管理员 </p>
-// */
-//@Controller
-//@RequestMapping("/player")
-//@Slf4j
-//public class PlayerController {
-//
-//    private PlayerService playerService;
-//
-//    @GetMapping("/index")
-//    @RequiresAuthentication
-//    public String index() {
-//        log.info("进入玩家设置页面");
-//        return "/player/index";
-//    }
-//
-//    @GetMapping("/getDstAdminList")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<List<String>> getDstAdminList(@RequestParam(required = true) String roomId) {
-//        log.info("拉取管理员列表");
+package com.tugos.dst.admin.controller;
+
+import com.tugos.dst.admin.common.ResultVO;
+import com.tugos.dst.admin.service.PlayerService;
+import com.tugos.dst.admin.vo.PlayerSettingVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.authz.annotation.RequiresAuthentication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author qinming
+ * @date 2020-11-16 22:46:07
+ * <p> 玩家设置，设置黑白名单，管理员 </p>
+ */
+@Controller
+@RequestMapping("/player")
+@Slf4j
+public class PlayerController {
+
+    private PlayerService playerService;
+
+    @GetMapping("/index")
+    @RequiresAuthentication
+    public String index() {
+        log.info("进入玩家设置页面");
+        return "/player/index";
+    }
+
+    @GetMapping("/getDstAdminList")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<List<String>> getDstAdminList(@RequestParam(required = true) String roomId) {
+        log.info("拉取管理员列表");
 //        return ResultVO.data(playerService.getDstAdminList(roomId));
-//    }
-//
-//    @GetMapping("/getDstBlacklist")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<List<String>> getDstBlacklist(@RequestParam(required = true) String roomId) {
-//        log.info("拉取玩家黑名单列表");
+        return ResultVO.success();
+    }
+
+    @GetMapping("/getDstBlacklist")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<List<String>> getDstBlacklist(@RequestParam(required = true) String roomId) {
+        log.info("拉取玩家黑名单列表");
 //        return ResultVO.data(playerService.getDstBlacklist(roomId));
-//    }
-//
-//    @PostMapping("/saveAdminList")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    @Deprecated
-//    public ResultVO<String> saveAdminList(@RequestBody List<String> adminList,@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("保存管理员：" + adminList);
+        return ResultVO.success();
+    }
+
+    @PostMapping("/saveAdminList")
+    @RequiresAuthentication
+    @ResponseBody
+    @Deprecated
+    public ResultVO<String> saveAdminList(@RequestBody List<String> adminList,@RequestParam(required = true) String roomId) throws Exception {
+        log.info("保存管理员：" + adminList);
 //        return playerService.saveAdminList(adminList,roomId);
-//    }
-//
-//    @PostMapping("/saveBlackList")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    @Deprecated
-//    public ResultVO<String> saveBlackList(@RequestBody List<String> blackList,@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("保存黑名单：" + blackList);
+        return ResultVO.success();
+    }
+
+    @PostMapping("/saveBlackList")
+    @RequiresAuthentication
+    @ResponseBody
+    @Deprecated
+    public ResultVO<String> saveBlackList(@RequestBody List<String> blackList,@RequestParam(required = true) String roomId) throws Exception {
+        log.info("保存黑名单：" + blackList);
 //        return playerService.saveBlackList(blackList,roomId);
-//    }
-//
-//    @PostMapping("/saveAdminAndBlackList")
-//    @RequiresAuthentication
-//    @ResponseBody
-//    public ResultVO<String> saveAdminAndBlackList(@RequestBody PlayerSettingVO playerSettingVO,@RequestParam(required = true) String roomId) throws Exception {
-//        log.info("保存管理员和黑名单：" + playerSettingVO);
+        return ResultVO.success();
+    }
+
+    @PostMapping("/saveAdminAndBlackList")
+    @RequiresAuthentication
+    @ResponseBody
+    public ResultVO<String> saveAdminAndBlackList(@RequestBody PlayerSettingVO playerSettingVO,@RequestParam(required = true) String roomId) throws Exception {
+        log.info("保存管理员和黑名单：" + playerSettingVO);
 //        playerService.saveAdminList(playerSettingVO.getAdminList(),roomId);
 //        playerService.saveBlackList(playerSettingVO.getBlackList(),roomId);
-//        return ResultVO.success();
-//    }
-//
-//
-//    @Autowired
-//    public void setPlayerService(PlayerService playerService) {
-//        this.playerService = playerService;
-//    }
-//}
+        return ResultVO.success();
+    }
+
+
+    @Autowired
+    public void setPlayerService(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+}
